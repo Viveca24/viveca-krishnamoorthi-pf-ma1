ae8c7cbed23a991a2d47cf41be60caac1c0f3578
Viveca Krishnamoorthi <vivve_92@hotmail.com>
2020-09-25 18:03:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// QUESTION 1
+
+var firstVar;
+
+
+// QUESTION 2
+
+var name = "viveca";
+console.log(name);
+
+
+// QUESTION 3
+
+var vivecaAge = 27;
+console.log(vivecaAge)
+
+
+// QUESTION 4
+
+var division = 20 / 5;
+// division; 
+    4
+
+
+// QUESTION 5 
+
+typeof "frog";
+// "string"
+
+
+// QUESTION 6
+
+var orderHasShipped = true;
+
+
+// QUESTION 7
+
+if (orderHasShipped === true) {
+    console.log("true")
+} else {
+    console.log("false");
+}
+
+
+// QUESTION 8
+
+for(var i = 0; i <= 9; i++) {
+    console.log(i);
+}
